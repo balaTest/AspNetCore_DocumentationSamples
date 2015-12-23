482ac0186aa255499b025092548e15074baee4ff
Bala Chirtsabesan <balatest@live.com>
2015-12-22 18:44:33 -0800

    [ConfigDemo] UsingOptions sample : Configure MyOptions using config

--- Fundamentals/Configuration/ConfigDemo/src/UsingOptions/Startup.cs
+++ Fundamentals/Configuration/ConfigDemo/src/UsingOptions/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UsingOptions.Models;
 
@@ -7,6 +8,17 @@ namespace UsingOptions
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -14,11 +26,13 @@ namespace UsingOptions
             // Setup options with DI
             services.AddOptions();
 
+            // Configure MyOptions using config
+            services.Configure<MyOptions>(Configuration);
+            
             // Configure MyOptions using code
             services.Configure<MyOptions>(myOptions =>
             {
                 myOptions.Option1 = "value1_from_action";
-                myOptions.Option2 = 2;
             });
 
             // Add framework services.
