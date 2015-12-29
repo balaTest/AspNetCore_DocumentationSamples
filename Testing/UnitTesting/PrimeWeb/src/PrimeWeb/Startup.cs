73f9b652e67a9f54f2ac32cd20059330493fea1c
Bala Chirtsabesan <balatest@live.com>
2015-12-28 17:01:20 -0800

    [UnitTesting_PrimeWeb] Add test project with unit tests

--- Testing/UnitTesting/PrimeWeb/src/PrimeWeb/Startup.cs
+++ Testing/UnitTesting/PrimeWeb/src/PrimeWeb/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using PrimeWeb.Services;
 
 namespace PrimeWeb
 {
@@ -24,7 +21,31 @@ namespace PrimeWeb
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Path.Value.Contains("checkprime"))
+                {
+                    int numberToCheck;
+                    try
+                    {
+                        numberToCheck = int.Parse(context.Request.QueryString.Value.Replace("?", ""));
+                        var primeService = new PrimeService();
+                        if (primeService.IsPrime(numberToCheck))
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is prime!");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is NOT prime!");
+                        }
+                    }
+                    catch
+                    {
+                        await context.Response.WriteAsync("Pass in a number to check in the form /checkprime?5");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                }
             });
         }
 
