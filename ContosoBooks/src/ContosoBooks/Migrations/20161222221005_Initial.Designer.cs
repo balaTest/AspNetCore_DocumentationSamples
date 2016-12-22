2980c4c5722c5db406057c2199182f02cf622637
Bala Chirtsabesan <balaTest@live.com>
2016-12-22 14:18:50 -0800

    [ContosoBooks] Add Books and Authors with EF and migrations

---
+++ ContosoBooks/src/ContosoBooks/Migrations/20161222221005_Initial.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ContosoBooks.Models;
+
+namespace ContosoBooks.Migrations
+{
+    [DbContext(typeof(ContosoBooksDbContext))]
+    [Migration("20161222221005_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ContosoBooks.Models.Author", b =>
+                {
+                    b.Property<int>("AuthorID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstMidName");
+
+                    b.Property<string>("LastName")
+                        .IsRequired();
+
+                    b.HasKey("AuthorID");
+
+                    b.ToTable("Author");
+                });
+
+            modelBuilder.Entity("ContosoBooks.Models.Book", b =>
+                {
+                    b.Property<int>("BookID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AuthorID");
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<string>("Title")
+                        .IsRequired();
+
+                    b.Property<int>("Year");
+
+                    b.HasKey("BookID");
+
+                    b.HasIndex("AuthorID");
+
+                    b.ToTable("Book");
+                });
+
+            modelBuilder.Entity("ContosoBooks.Models.Book", b =>
+                {
+                    b.HasOne("ContosoBooks.Models.Author", "Author")
+                        .WithMany("Books")
+                        .HasForeignKey("AuthorID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
